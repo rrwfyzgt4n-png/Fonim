c375e2633b497b857c997d2d5d8b572f001dc8cb
rrwfyzgt4n-png <rrwfyzgt4n@privaterelay.appleid.com>
2026-06-12 12:23:09 -0400

    Add available voice presets

--- Sources/VibeVoiceBatchCore/Support/AppDefaults.swift
+++ Sources/VibeVoiceBatchCore/Support/AppDefaults.swift
@@ -4,8 +4,35 @@ public enum AppDefaults {
     public static let projectRoot = URL(fileURLWithPath: "/Volumes/FastData/vibevoice-docker", isDirectory: true)
     public static let dockerImage = "vibevoice-cpu"
     public static let modelPath = "microsoft/VibeVoice-Realtime-0.5B"
-    public static let defaultVoice = "en-carter"
+    public static let defaultVoice = "en-carter_man"
     public static let defaultCFGScale = "1.8"
+    public static let availableVoices = [
+        "de-spk0_man",
+        "de-spk1_woman",
+        "en-carter_man",
+        "en-davis_man",
+        "en-emma_woman",
+        "en-frank_man",
+        "en-grace_woman",
+        "en-mike_man",
+        "fr-spk0_man",
+        "fr-spk1_woman",
+        "in-samuel_man",
+        "it-spk0_woman",
+        "it-spk1_man",
+        "jp-spk0_man",
+        "jp-spk1_woman",
+        "kr-spk0_woman",
+        "kr-spk1_man",
+        "nl-spk0_man",
+        "nl-spk1_woman",
+        "pl-spk0_man",
+        "pl-spk1_woman",
+        "pt-spk0_woman",
+        "pt-spk1_man",
+        "sp-spk0_woman",
+        "sp-spk1_man"
+    ]
 }
 
 public extension URL {
